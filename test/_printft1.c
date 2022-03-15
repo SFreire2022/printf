f2f7ed6ce5b35985ae77b9d1aa3a34006c6f9b62
SFreire2022 <sebastiancazenave@gmail.com>
2022-03-14 23:13:03 -0300

    change index increment after handle_modificators

--- test/_printft1.c
+++ test/_printft1.c
@@ -181,6 +181,7 @@ int _printf(const char *format, ...)
 			if (h_count == -1) /* If error retturn -1 and exit form _printf function */
 				return (-1);/* check if need to frees memory before exit */
 			c_count += h_count;
+			i++;
 		}
 	}
 	write_buffer(buffer, buff_pos);
