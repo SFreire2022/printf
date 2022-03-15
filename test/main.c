6d2b5f3aee2aa5681c02b2755ff6c6706a29f8d2
SFreire2022 <sebastiancazenave@gmail.com>
2022-03-15 19:05:56 -0300

    insigned int to index

--- test/main.c
+++ test/main.c
@@ -13,7 +13,7 @@ int main(void)
     int len2;
     unsigned int ui;
     void *addr;
-	unsigned char uschar = INT_MAX + 1024;
+	unsigned char uschar = INT_MAX;
 
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
