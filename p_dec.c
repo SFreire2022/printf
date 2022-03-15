a53aa4ff843c9c475c615e02312e7b4bdd95724d
SFreire2022 <sebastiancazenave@gmail.com>
2022-03-15 02:45:04 -0300

    pass p_dec from test to pre prod

---
+++ p_dec.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * p_dec - Add given number to buffer
+ * @ap: char
+ * @buffer: Buffer holding string to print
+ * @length: Position in buffer
+ * Return: number of chars written to buff
+ */
+int p_dec(va_list ap, char *buffer, int *length)
+{
+	unsigned int a, b, c, d, f;
+	int n;
+	int count = 0;
+
+	n = va_arg(ap, int);
+	if (n < 0)
+	{
+		count = addc2buff('-', buffer, length);
+		n = n * -1;
+	}
+	b = n;
+	c = 1;
+	for (a = 1 ; b > 9 ; a++)
+	{
+		b = b / 10;
+		c = c * 10;
+	}
+	for (f = 1; f <= a; f++)
+	{
+		d = n / c;
+		n = n % c;
+		c = c / 10;
+		count += addc2buff(d + '0', buffer, length);
+	}
+	return (count);
+}
